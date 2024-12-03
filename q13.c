3f04f41c7e15b578d75c5dbb33fe8d0d5663b781
liuz-svg <luizhenrique0308@gmail.com>
2024-12-03 15:53:48 -0300

    Delete q13.c

--- q13.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    unsigned char C;
-    unsigned char F;
-    puts("escolha o valor em celsius");
-    scanf("%hhu", &C);
-    printf("o valor escolhido em fahrenheit e: %hhu", (9*C+160)/5);
-}
