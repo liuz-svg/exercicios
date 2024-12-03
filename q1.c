cf107e9fef8c991adb34d6a7511e189dcd86e22c
liuz-svg <luizhenrique0308@gmail.com>
2024-12-03 15:52:12 -0300

    Delete q1.c

--- q1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int a;
-  printf("escolha o valor de a:");
-  scanf("%d", &a);
-  printf("o valor de a em hexadecimal é %x\n e o valor em octal é %o", a, a);
-}
