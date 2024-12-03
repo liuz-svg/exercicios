efea06947210b9f2280cd88744a0f75b54b5fb98
liuz-svg <luizhenrique0308@gmail.com>
2024-12-03 15:58:15 -0300

    Delete q3.c q4.c

--- q3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int a;
-  printf("escolha o valor:");
-  scanf("%d", &a);
-  printf("o triplo do valor é %d, o seu quadrado é %d, e sua metade é %d",
-         3 * a, a * a, a / 2);
-}
