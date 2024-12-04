c96c4d6c96c503cb2d1527fc9fb5bfe79dda54ec
liuz-svg <luizhenrique0308@gmail.com>
2024-12-03 21:06:14 -0300

    Add files via upload

---
+++ lista3/q4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#define TAM 30
+int main(void){
+    char frase[TAM], frase2[TAM], j[TAM];
+    puts("escreva uma frase:");
+    fgets(frase, TAM, stdin);
+    puts("escreva a segunda frase:");
+    fgets(frase2, TAM, stdin);
+   for(int i=0; i<TAM; i++){
+        if(frase[i] != '\n') 
+        j[i]= frase[i];
+    }
+    for(int i= 0; i<TAM; i++){
+
+    }
+    
+}
